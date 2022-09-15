1dd4da7c8ea7b028fd514fb6b824be5dcf75a811
TriekMan <9019629@student.zadkine.nl>
2022-09-15 14:56:58 +0200

    opdracht3.2js

--- app.js
+++ app.js
@@ -4,5 +4,20 @@ const myName = "Tarik";
 let myAge = 18;
 const diploma = true;
 let passed;
+document.write()
 
-document.write ()
+
+let num1 = 10;
+let num2 = 23;
+let answer = num1 * num2;
+document.write(answer)
+
+let answer1 = num1 + num2;
+document.write(answer1)
+
+let answer2 = num1 - num2;
+document.write(answer2)
+
+let answer3 = num1 / num2;
+document.write(answer3)
+
