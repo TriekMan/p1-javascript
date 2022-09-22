8540567c1576975a941de5c38ba6cbfde9e36e60
TriekMan <9019629@student.zadkine.nl>
2022-09-22 15:30:16 +0200

    jvsopdracht

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 //var -> let
 
-const myName = "Tarik";
-let myAge = 18;
-const diploma = true;
-let passed;
-document.write()
+//const myName = "Tarik";
+//let myAge = 18;
+//const diploma = true;
+//let passed;
+//document.write()
 
 
-let num1 = 10;
-let num2 = 23;
-let answer = num1 * num2;
-document.write(answer)
+//let num1 = 10;
+//let num2 = 23;
+//let answer = num1 * num2;
+//document.write(answer)
 
-let answer1 = num1 + num2;
-document.write(answer1)
+//let answer1 = num1 + num2;
+//document.write(answer1)
 
-let answer2 = num1 - num2;
-document.write(answer2)
+//let answer2 = num1 - num2;
+//document.write(answer2)
 
-let answer3 = num1 / num2;
-document.write(answer3)
+//let answer3 = num1 / num2;
+//document.write(answer3)
 
