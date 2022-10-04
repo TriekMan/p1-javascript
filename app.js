700d9120a9f5b8fcceaeb09c782577054b6f6b70
TriekMan <9019629@student.zadkine.nl>
2022-10-04 11:44:43 +0200

    opdracht5.1

--- app.js
+++ app.js
@@ -1,23 +1,11 @@
-//var -> let
+// var -> let
 
-//const myName = "Tarik";
-//let myAge = 18;
-//const diploma = true;
-//let passed;
-//document.write()
+document.getElementById("header1").style.color = "red";
+
+document.getElementById("header1").innerText = "het werkt"
 
 
-//let num1 = 10;
-//let num2 = 23;
-//let answer = num1 * num2;
-//document.write(answer)
 
-//let answer1 = num1 + num2;
-//document.write(answer1)
 
-//let answer2 = num1 - num2;
-//document.write(answer2)
 
-//let answer3 = num1 / num2;
-//document.write(answer3)
 
