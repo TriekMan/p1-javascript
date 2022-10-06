1d07f9f1c4a52fb4eb720a6c3f3b4e03ea90fa5f
TriekMan <9019629@student.zadkine.nl>
2022-10-06 15:00:57 +0200

    opdracht6js

---
+++ opdracht_6.4/app.js
@@ -0,0 +1,23 @@
+//Oefening1---
+function calculate(num1, num2) {
+    let answer = num1 + num2;
+    console.log(answer)
+}
+calculate(289, 2);
+
+
+//Oefening2---
+// function multiplyByFive(num1) {
+//     let answer = num1 * 5;
+//     console.log(answer)
+// }
+// multiplyByFive(10)
+
+
+//Oefening3---
+// function calculateMinutes(num1) {
+//     let answer = num1 / 60;
+//     console.log(answer)
+// }
+// calculateMinutes(360)
+
